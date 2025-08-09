5a5c23495f09e493c6f8b2de6124c9510b54f295
PMinn <40393799+PMinn@users.noreply.github.com>
2025-08-09 23:51:17 +0800

    改善中文 UI/UX

---
+++ pages/menu.js
@@ -0,0 +1,10 @@
+const menuPages = document.querySelectorAll('#menu>div');
+
+document.querySelectorAll('.nav-link').forEach((link, index) => {
+    link.addEventListener('click', () => {
+        document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+        menuPages.forEach(l => l.classList.remove('active'));
+        menuPages[index].classList.add('active');
+    });
+});
